d6b290c4a3811f6b95b5c6f079b170115e228062
Ezra <ezrabathini@gmail.com>
2017-04-11 23:53:14 +1200

    first commit

---
+++ Pods/Target Support Files/Pods-nowReading/Pods-nowReading-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nowReading : NSObject
+@end
+@implementation PodsDummy_Pods_nowReading
+@end
---
+++ Pods/Target Support Files/Pods-nowReadingUITests/Pods-nowReadingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nowReadingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_nowReadingUITests
+@end
